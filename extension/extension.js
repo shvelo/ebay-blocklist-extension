1058099078b694a675a57c0f647100ba777ab6a2
Nick Shvelidze <captain@pirrate.me>
2020-04-27 14:00:48 +0400

    Add notification and undo

--- extension/extension.js
+++ extension/extension.js
@@ -1,3 +1,7 @@
+const buttonClass = 'blacklist-extension-button';
+const notificationClass = 'blacklist-extension-notification';
+const notificationButtonClass = 'blacklist-extension-notification-button';
+
 var blacklist = [];
 
 init();
@@ -38,7 +42,42 @@ function blacklistSeller(seller, cb) {
     if (isBlacklisted(seller))
         return;
     blacklist.push(seller);
+    notifyBlacklisted(seller);
     saveBlacklist(cb);
+}
+
+function unblacklistSeller(seller, cb) {
+    let index = blacklist.indexOf(seller);
+    if (index !== -1) {
+        blacklist.splice(index, 1);
+    }
+    saveBlacklist(cb);
+}
+
+function notifyBlacklisted(seller) {
+    let removed = false;
+    blacklistNotification = document.createElement('div');
+    blacklistNotification.innerHTML = `Seller <strong>${seller}</strong> added to blacklist`;
+    blacklistNotification.className = notificationClass;
+    const undoButton = document.createElement('button');
+    undoButton.innerText = 'undo';
+    undoButton.className = notificationButtonClass;
+    let cachedSeller = seller;
+    undoButton.addEventListener('click', function () {
+        unblacklistSeller(cachedSeller);
+        if (!removed) {
+            document.body.removeChild(blacklistNotification);
+            removed = true;
+        }
+    });
+    blacklistNotification.appendChild(undoButton);
+    document.body.appendChild(blacklistNotification);
+    setTimeout(function () {
+        if (!removed) {
+            document.body.removeChild(blacklistNotification);
+            removed = true;
+        }
+    }, 5000);
 }
 
 function runBlacklist() {
@@ -52,18 +91,20 @@ function runBlacklist() {
 
         if (isBlacklisted(seller)) {
             console.log('Found result from blacklisted seller', seller);
-            result.parentElement.removeChild(result);
+            result.style.display = 'none';
         } else {
+            result.style.display = 'block';
             // Add blacklist button to all results
             sellerArea = result.querySelector('.lvdetails li:last-child');
 
             // Return if button already added
-            if (sellerArea.lastChild && sellerArea.lastChild.dataset && sellerArea.lastChild.dataset.isBlacklistButton)
+            if (sellerArea.lastChild && sellerArea.lastChild.className === buttonClass)
                 return;
 
             blacklistButton = document.createElement('button');
-            blacklistButton.innerText = 'Blacklist';
+            blacklistButton.innerHTML = '&times Blacklist';
             blacklistButton.dataset.isBlacklistButton = true;
+            blacklistButton.className = buttonClass;
             let cachedSeller = seller;
             blacklistButton.addEventListener('click', function () {
                 blacklistSeller(cachedSeller);
